0359d5f645aceb7ac75abccc1d182547486b7045
G-Hae <sifialee@gmail.com>
2023-01-17 23:12:01 +0900

    feat: theme(다크모드 변경) 기초 + 타입스크립트

--- reactmaster/src/App.tsx
+++ reactmaster/src/App.tsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
 
-  const [value, setValue] = useState("")
+  const Container = styled.div`
+  background-color: ${(props)=>props.theme.bgColor};
+    
+  `
 
+  const H1 = styled.h1`
+  color: ${(props)=>props.theme.textColor};
+  `
+
+  const [value, setValue] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+    };
+
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    console.log("hello", value)
+  };
 
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="username"/>
+    <Container>
+      <H1>Hi Everybody</H1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={value} type="text" placeholder="username"/>
         <button>LOG IN</button>
       </form>
-    </div>
-
+    </Container>
   )
 }
 
--- reactmaster/src/index.tsx
+++ reactmaster/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { darkTheme, lightTheme } from './theme';
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
