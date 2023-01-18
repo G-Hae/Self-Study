8720d7ad6cda99418a420d6b800de27824bff939
G-Hae <sifialee@gmail.com>
2023-01-18 09:18:36 +0900

    feat: 리액트 라우터 공부시작

--- reactmaster/src/index.tsx
+++ reactmaster/src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { darkTheme, lightTheme } from './theme';
+import { theme } from './theme';
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
