668284564543ae78d27141593123492e943285e4
G-Hae <sifialee@gmail.com>
2023-01-18 22:49:31 +0900

    feat:1월 18일 공부정리

--- reactmaster/src/routes/Coin.tsx
+++ reactmaster/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -38,6 +38,19 @@ function Coin(){
     const [loading, setLoading] = useState(true);
     const {coinId} = useParams<RouteParams>();
     const {state} = useLocation<RouteState>();
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo0] = useState({});
+
+    useEffect(()=>{
+        (async()=>{
+            const infoData = await(
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+            const priceData = await(
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);
+            setPriceInfo0(priceData);
+        }) ();
+    }, [])
 
     return(
     <Container>
