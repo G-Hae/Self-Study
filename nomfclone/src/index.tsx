fecc568bed34914827bb942f9609eb0ba962d251
G-Hae <sifialee@gmail.com>
2023-03-04 16:29:53 +0900

    feat: main css / data fetch

--- nomfclone/src/index.tsx
+++ nomfclone/src/index.tsx
@@ -4,6 +4,7 @@ import { RecoilRoot } from 'recoil';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './theme';
+import { QueryClient, QueryClientProvider } from "react-query"
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -58,7 +59,11 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
     body{
+        font-weight: 300;
         font-family: 'Source Sans Pro', sans-serif;
+        color: ${props=>props.theme.white.darker};
+        line-height: 1.2;
+        background-color: black;
     }
     a{
         text-decoration: none;
@@ -66,13 +71,17 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 
+const client = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-          <GlobalStyle/>
-          <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle/>
+            <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
